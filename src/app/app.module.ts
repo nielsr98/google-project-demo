9b4538125677fc4ae55d703c82c46a1e82078767
niels <nielsr@seas.upenn.edu>
2020-08-17 11:33:37 -0400

    build(demo): initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoHomeComponent } from './demo-home/demo-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
